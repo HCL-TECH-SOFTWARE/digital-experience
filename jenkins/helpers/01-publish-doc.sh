8444f5f65784e8d08afbfb17523c8056cef339ff
Philipp Milich <philpp.milich@hcl.com>
2022-03-25 11:29:25 +0100

    added execution files

---
+++ jenkins/helpers/01-publish-doc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install dependencies
+microdnf install -y --nodocs git python3
+
+# Install mkdocs
+pip3 install mkdocs-material mkdocs-awesome-pages-plugin mkdocs-git-revision-date-localized-plugin
+
+# Chmod private key for Git Access
+chmod -R 600 /root/.ssh/id_rsa
+
+# Add Git hosts for SSH
+ssh-keyscan git.cwp.pnp-hcl.com >> /root/.ssh/known_hosts
+
+# Perform clone of target repository
+git clone git@git.cwp.pnp-hcl.com:CWPdoc/dx-mkdocs.git
+
+# Perform GH pages deploy
+mkdocs gh-deploy --force
